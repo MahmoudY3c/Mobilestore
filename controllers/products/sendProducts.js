2d93ae3664fd52ea36dc9d2c76efe77cd8350e7a
MahmoudY3c <wpsmahmoud25@gmail.com>
2024-02-06 21:52:46 +0200

    added products search handler

--- controllers/products/sendProducts.js
+++ controllers/products/sendProducts.js
@@ -2,10 +2,23 @@ const Products = require('../../db/models/Products');
 const { asyncHandler } = require('../../handlers/error');
 
 const sendProducts = asyncHandler(async (req, res) => {
-  const { category, skip = 0, limit = 14 } = req.query;
+  const { category, q, skip = 0, limit = 14 } = req.query;
   const query = {};
   if (category) {
     query.category = category;
+  }
+
+  // handling search
+  if (q) {
+    const search = {
+      name: {
+        $regex: new RegExp(q, 'gi'),
+      },
+    };
+
+    const products = await Products.find(search, {}, { skip: Number(skip), limit: Number(limit) });
+    const numberOfProducts = await Products.countDocuments(search);
+    return res.status(200).json({ data: products, length: numberOfProducts });
   }
 
   // console.log(req.query);
