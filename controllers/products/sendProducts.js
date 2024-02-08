2e4fb35999468715bd8b2c133f161913c6701354
MahmoudY3c <wpsmahmoud25@gmail.com>
2024-02-08 22:14:01 +0200

    added a new search option to send only product name for fast response

--- controllers/products/sendProducts.js
+++ controllers/products/sendProducts.js
@@ -2,7 +2,7 @@ const Products = require('../../db/models/Products');
 const { asyncHandler } = require('../../handlers/error');
 
 const sendProducts = asyncHandler(async (req, res) => {
-  const { category, q, skip = 0, limit = 14 } = req.query;
+  const { category, q, name, skip = 0, limit = 14 } = req.query;
   const query = {};
   if (category) {
     query.category = category;
@@ -16,7 +16,7 @@ const sendProducts = asyncHandler(async (req, res) => {
       },
     };
 
-    const products = await Products.find(search, {}, { skip: Number(skip), limit: Number(limit) });
+    const products = await Products.find(search, name ? { name: 1 } : {}, { skip: Number(skip), limit: Number(limit) });
     const numberOfProducts = await Products.countDocuments(search);
     return res.status(200).json({ data: products, length: numberOfProducts });
   }
