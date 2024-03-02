9593e9b6acbc8b033c935af6b6e2bd46d9755a38
MahmoudY3c <wpsmahmoud25@gmail.com>
2024-03-02 21:31:06 +0200

    a dummy commit for fix simple issues related to render - Sat 03/02/2024 21:31:03.40

--- controllers/messages/sendMessages.js
+++ controllers/messages/sendMessages.js
@@ -26,7 +26,7 @@ const sendMessages = asyncHandler(async (req, res) => {
 
   const msgs = await Messages.find(query, {}, { skip: Number(skip), limit: Number(limit) });
 
-  console.log(query, msgs);
+  console.log(query, role, msgs);
 
   const numberOfProducts = await Messages.countDocuments(query);
   res.status(200).json({ data: msgs, length: numberOfProducts });
